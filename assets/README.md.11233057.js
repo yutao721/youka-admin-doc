c11e24811d54b98f93196f18353765ea70d72209
yutao <642231346@qq.com>
2022-11-10 17:16:22 +0800

    deploy

--- assets/README.md.11233057.js
+++
@@ -1,9 +0,0 @@
-import{_ as n,f as a,e,N as s}from"./plugin-vue_export-helper.87254cb5.js";const f='{"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"}],"relativePath":"README.md","lastUpdated":1666179419139}',t={},o=s(`<h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1"><a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a> \u5982\u4F55\u672C\u5730\u5F00\u53D1</h2><div class="language-bash"><pre><code><span class="token comment"># \u514B\u9686\u672C\u4ED3\u5E93</span>
-$ <span class="token function">git</span> clone git@github.com:yutao721/youka-admin-doc.git
-
-<span class="token comment"># \u6216\u8005\u4F7F\u7528 yarn</span>
-$ <span class="token function">yarn</span> <span class="token function">install</span>
-
-<span class="token comment"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span>
-$ <span class="token function">yarn</span> dev
-</code></pre></div>`,2),c=[o];function r(i,p,_,l,d,u){return e(),a("div",null,c)}var h=n(t,[["render",r]]);export{f as __pageData,h as default};
---
+++ assets/README.md.9df44ed1.js
@@ -0,0 +1,7 @@
+import{_ as a,f as n,e,N as s}from"./plugin-vue_export-helper.87254cb5.js";const u='{"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"}],"relativePath":"README.md","lastUpdated":1668070717348}',t={},o=s(`<h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1"><a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a> \u5982\u4F55\u672C\u5730\u5F00\u53D1</h2><div class="language-bash"><pre><code>
+<span class="token comment"># \u6216\u8005\u4F7F\u7528 yarn</span>
+$ <span class="token function">yarn</span> <span class="token function">install</span>
+
+<span class="token comment"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span>
+$ <span class="token function">yarn</span> dev
+</code></pre></div>`,2),c=[o];function r(_,l,p,d,i,f){return e(),n("div",null,c)}var m=a(t,[["render",r]]);export{u as __pageData,m as default};
