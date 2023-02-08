3bade95ff33cc70a1c1c87bf403bd92fa7eb5303
余涛 <642231346@qq.com>
2023-02-08 19:29:08 +0800

    deploy

--- assets/README.md.71437892.lean.js
+++
@@ -1,7 +0,0 @@
-import{_ as a,f as e,e as n,N as s}from"./plugin-vue_export-helper.87254cb5.js";const u='{"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"},{"level":2,"title":"\u5982\u4F55","slug":"\u5982\u4F55"}],"relativePath":"README.md","lastUpdated":1675855261417}',t={},c=s(`__VP_STATIC_START__<h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1"><a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a> \u5982\u4F55\u672C\u5730\u5F00\u53D1</h2><div class="language-bash"><pre><code>
-<span class="token comment"># \u6216\u8005\u4F7F\u7528 yarn</span>
-$ <span class="token function">yarn</span> <span class="token function">install</span>
-
-<span class="token comment"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span>
-$ <span class="token function">yarn</span> dev
-</code></pre></div><h2 id="\u5982\u4F55"><a class="header-anchor" href="#\u5982\u4F55" aria-hidden="true">#</a> \u5982\u4F55</h2>__VP_STATIC_END__`,3),o=[c];function r(l,_,d,i,p,h){return n(),e("div",null,o)}var m=a(t,[["render",r]]);export{u as __pageData,m as default};
