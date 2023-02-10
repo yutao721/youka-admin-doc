d2ec9767006931a98adc8b3abffa0060d807aae5
余涛 <642231346@qq.com>
2023-02-10 11:28:49 +0800

    deploy

--- assets/README.md.212a4a57.lean.js
+++
@@ -1,7 +0,0 @@
-import{_ as a,f as e,e as s,N as n}from"./plugin-vue_export-helper.87254cb5.js";const g='{"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"},{"level":2,"title":"\u5982\u4F55\u5C06\u6587\u6863\u90E8\u7F72\u5230github-pages?","slug":"\u5982\u4F55\u5C06\u6587\u6863\u90E8\u7F72\u5230github-pages"}],"relativePath":"README.md","lastUpdated":1675855721598}',t={},o=n(`__VP_STATIC_START__<h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1"><a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a> \u5982\u4F55\u672C\u5730\u5F00\u53D1</h2><div class="language-bash"><pre><code>
-<span class="token comment"># \u6216\u8005\u4F7F\u7528 yarn</span>
-$ <span class="token function">yarn</span> <span class="token function">install</span>
-
-<span class="token comment"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span>
-$ <span class="token function">yarn</span> dev
-</code></pre></div><h2 id="\u5982\u4F55\u5C06\u6587\u6863\u90E8\u7F72\u5230github-pages"><a class="header-anchor" href="#\u5982\u4F55\u5C06\u6587\u6863\u90E8\u7F72\u5230github-pages" aria-hidden="true">#</a> \u5982\u4F55\u5C06\u6587\u6863\u90E8\u7F72\u5230github-pages?</h2><p>\u6267\u884C\u6839\u76EE\u5F55\u4E0B\u7684deploy.sh\u811A\u672C\u5373\u53EF</p>__VP_STATIC_END__`,4),c=[o];function r(i,p,l,d,_,h){return s(),e("div",null,c)}var f=a(t,[["render",r]]);export{g as __pageData,f as default};
